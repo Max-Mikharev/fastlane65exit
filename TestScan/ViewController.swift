10238955a92f5cbe61a145a3d4700082f3d0f535
Maksim Mikharev <maxim.mikharev@team.wrike.com>
2018-11-28 23:51:53 +0300

    Initial Commit

---
+++ TestScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestScan
+//
+//  Created by Maxim Mikharev on 28/11/2018.
+//  Copyright © 2018 Maxim Mikharev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
